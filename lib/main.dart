4cef1ab39ab60e7894fff8afaf8e4670dce9430c
Nikita Ptashkin <nikita.ptashckin@yandex.ru>
2025-09-16 13:32:36 +0300

    pub: hive добавил

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gtd_manager/app/app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const GtdManager());
 }
